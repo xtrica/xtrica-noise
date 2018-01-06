89a04f97da9845d0d833893b1edd51a59c72197e
xtrica <xtrica.com@gmail.com>
2018-01-05 22:26:27 -0500

    v1.0.1515209187

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 /*
- * xtrica-noise v1.0.1514928579 (https://xtrica.com)
+ * xtrica-noise v1.0.1515209187 (https://xtrica.com)
  * Copyright 2017-2018 (or 2150?) Xtrica
  * Licensed under MIT
  */
 export default function(){this.noises={}
 this.play=function(key,volume=-1){let vm=this
 return new Promise(function(resolve,reject){key=key.toLowerCase()
-if(vm.noises.hasOwnProperty(key)){try{vm.noises[key][0].addEventListener('ended',function handler(){vm.noises[key][0].currentTime=0
+if(vm.noises.hasOwnProperty(key)&&vm.noises[key][2]===!0){try{vm.noises[key][0].addEventListener('ended',function handler(){vm.noises[key][0].currentTime=0
 vm.noises[key][0].removeEventListener('ended',handler)
 resolve()})
 vm.noises[key][0].volume=(volume>-1)?(Math.abs(parseInt(volume))/100):vm.noises[key][1]
 vm.noises[key][0].play()}catch(error){resolve()}}else{resolve()}})}
-this.add=function(key,audioObject,defaultVolume=100){this.noises[key.toLowerCase()]=[audioObject,(Math.abs(parseInt(defaultVolume))/100)]
-this.noises[key.toLowerCase()][0].load()}}
+this.add=function(key,audioObject,defaultVolume=100){this.noises[key.toLowerCase()]=[audioObject,(Math.abs(parseInt(defaultVolume))/100),!1]
+vm.noises[key][0].addEventListener('canplaythrough',function(){vm.noises[key][2]=!0})
+vm.noises[key][0].addEventListener('unhandledrejection',function(error){throw error})}}
